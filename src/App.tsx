f6d918d5b4f45063547b863eaff0d3a312e42639
Csaba Darazs <42016191+Drammaster@users.noreply.github.com>
2026-03-06 02:39:31 +1300

    Formatting and added top right corner menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,76 @@
 import {
-    AppBar,
-    Button,
-    Toolbar,
-    Typography
-} from '@mui/material';
-import { ReactElement, useEffect } from 'react';
-import About from './components/about-me';
-import './styles/style.css';
-import { InsertDriveFile } from '@mui/icons-material';
-import Skills from './components/skills/skills';
+  AppBar,
+  Button,
+  Fade,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { ReactElement, useEffect } from "react";
+import About from "./components/about-me";
+import Skills from "./components/skills/skills";
+import "./styles/style.css";
+
+import MenuIcon from "@mui/icons-material/Menu";
 
 function visitorCount(): void {
-  fetch('https://api.countapi.xyz/update/drammaster.co.nz/visits/?amount=1');
+  fetch("https://api.countapi.xyz/update/drammaster.co.nz/visits/?amount=1");
 }
 
-
-
-
-
 function Header(): ReactElement {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Csaba Darazs
         </Typography>
-        <Button
-            variant='outlined'
-            startIcon={<InsertDriveFile />}
+        <div>
+          <Button
+            variant="outlined"
+            color="inherit"
+            id="fade-button"
+            startIcon={<MenuIcon />}
+            aria-controls={open ? "fade-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          />
+          <Menu
+            id="fade-menu"
+            slotProps={{
+              list: {
+                "aria-labelledby": "fade-button",
+              },
+            }}
+            slots={{ transition: Fade }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
+        {/* <Button
+          variant="outlined"
+          startIcon={<InsertDriveFile />}
           color="inherit"
           component="a"
           href="Csaba Darazs CV.pdf"
           target="_blank"
         >
           View Resume
-        </Button>
+        </Button> */}
       </Toolbar>
     </AppBar>
   );
